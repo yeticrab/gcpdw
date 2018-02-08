88d92971da8cd979ef13ac4aba4237a670247021
RogerGillTSM <roger.gill@moneysupermarket.com>
2018-02-08 11:19:46 +0000

    changed the schema

--- sql/schema/bq_schema.py
+++ sql/schema/bq_schema.py
@@ -1,8 +1,8 @@
 from google.cloud import bigquery
 # location table schema dictionary
-location = {}
-location['schema'] = (
-  bigquery.SchemaField('installationId', 'STRING')
+DeviceLocations = {}
+DeviceLocations['schema'] = (
+  bigquery.SchemaField('id', 'STRING')
   ,bigquery.SchemaField('timestamp', 'TIMESTAMP')
   ,bigquery.SchemaField('longitude', 'FLOAT')
   ,bigquery.SchemaField('latitude', 'FLOAT')
@@ -10,76 +10,70 @@ location['schema'] = (
   ,bigquery.SchemaField('speed', 'FLOAT')
   ,bigquery.SchemaField('course', 'FLOAT')
 )
-location['partitioning_type'] = "DAY"
+DeviceLocations['partitioning_type'] = "DAY"
+DeviceLocations['table_name'] = "device_locations"
 
 # visits
 visits = {}
 visits['schema'] = (
-  bigquery.SchemaField('venueId', 'STRING')
-  ,bigquery.SchemaField('installationId', 'STRING')
+  bigquery.SchemaField('venue_id', 'STRING')
+  ,bigquery.SchemaField('installation_id', 'STRING')
   ,bigquery.SchemaField('enter', 'TIMESTAMP')
   ,bigquery.SchemaField('exit', 'TIMESTAMP')
 )
 visits['partitioning_type'] = "DAY"
+visits['table_name'] = 'visits'
 
 # visits
 venues = {}
 venues['schema'] = (
-  bigquery.SchemaField('venueId', 'STRING')
+  bigquery.SchemaField('venue_id', 'STRING')
   ,bigquery.SchemaField('name', 'STRING')
   ,bigquery.SchemaField('longitude', 'FLOAT')
   ,bigquery.SchemaField('latitude', 'FLOAT')
 )
+venues['table_name'] = 'venues'
 
 # stepschallenge
 stepschallenge = {}
 stepschallenge['schema'] = (
-  bigquery.SchemaField('installationId', 'STRING')
+  bigquery.SchemaField('installation_id', 'STRING')
   ,bigquery.SchemaField('steps', 'INTEGER')
   ,bigquery.SchemaField('distance', 'FLOAT')
   ,bigquery.SchemaField('duration', 'FLOAT')
   ,bigquery.SchemaField('timestamp', 'TIMESTAMP')
-  ,bigquery.SchemaField('orgId', 'STRING')
+  ,bigquery.SchemaField('org_id', 'STRING')
 )
 stepschallenge['partitioning_type'] = "DAY"
+stepschallenge['table_name'] = 'stepschallenge'
 
 # organisations
 organisations = {}
 organisations['schema'] = (
-  bigquery.SchemaField('organisationId', 'STRING')
+  bigquery.SchemaField('organisation_id', 'STRING')
   ,bigquery.SchemaField('name', 'INTEGER')
   ,bigquery.SchemaField('distance', 'FLOAT')
   ,bigquery.SchemaField('duration', 'FLOAT')
   ,bigquery.SchemaField('timestamp', 'TIMESTAMP')
-  ,bigquery.SchemaField('orgId', 'STRING')
+  ,bigquery.SchemaField('org_id', 'STRING')
 )
 organisations['include'] = 0
+organisations['table_name'] = 'organisations'
 
-# organisations
-organisations = {}
-organisations['schema'] = (
-  bigquery.SchemaField('organisationId', 'STRING')
-  ,bigquery.SchemaField('name', 'INTEGER')
-  ,bigquery.SchemaField('distance', 'FLOAT')
-  ,bigquery.SchemaField('duration', 'FLOAT')
-  ,bigquery.SchemaField('timestamp', 'TIMESTAMP')
-  ,bigquery.SchemaField('orgId', 'STRING')
-)
-organisations['include'] = 0
-
-# organisations
+# installations
 installations = {}
 installations['schema'] = (
-  bigquery.SchemaField('id', 'STRING')
+  bigquery.SchemaField('installation_id', 'STRING')
   ,bigquery.SchemaField('idfa', 'STRING')
   ,bigquery.SchemaField('gender', 'STRING')
   ,bigquery.SchemaField('postcode', 'STRING')
   ,bigquery.SchemaField('dob', 'STRING')
   ,bigquery.SchemaField('join_date', 'TIMESTAMP')
   ,bigquery.SchemaField('username', 'STRING')
-  ,bigquery.SchemaField('installationId', 'STRING')
+  ,bigquery.SchemaField('id', 'STRING')
 )
 installations['include'] = 0
+installations['table_name'] = 'installations'
 
 
 # config for the job
