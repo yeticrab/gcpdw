1a8f4ceb8b4dec424bb4fb00eecfcfbf26425a26
RogerGillTSM <roger.gill@moneysupermarket.com>
2018-02-12 21:12:30 +0000

    added ability to skip existing tables

--- scripts/big_query_load.py
+++ scripts/big_query_load.py
@@ -10,10 +10,15 @@ pip install --upgrade google-cloud-storage
 '''
 
 import re
+from datetime     import datetime
 from google.cloud import storage
 from google.cloud import bigquery
 
+########################################
+########################################
+########################################
 
+# checks if table exisits
 def if_tbl_exists(client, table_ref):
     from google.cloud.exceptions import NotFound
     try:
@@ -21,8 +26,8 @@ def if_tbl_exists(client, table_ref):
         return True
     except NotFound:
         return False
-    
-# initiaties a data set
+
+# initiaties a data set - currently only the sojourn data
 def create_dataset(client, dataset_id):
     dataset_ref = client.dataset(dataset_id)
     dataset     = bigquery.Dataset(dataset_ref)
@@ -30,15 +35,18 @@ def create_dataset(client, dataset_id):
     
     return dataset
 
-def create_table_ref(client, blob, schema):
+def create_table_ref(client, dataset_id, blob, schema):
     file_type   = re.findall(r'/(.+)_', blob.name)[0]
     file_schema = schema[file_type]
     date        = ''
     if 'partitioning_type' in file_schema:
         match = re.search(r'\d{4}-\d{2}-\d{2}', blob.name)
-        date  = '_' + match.group()
+        date  = match.group()
+        date  = datetime.strptime(date,'%Y-%m-%d')
+        date  = date.strftime('%Y%m%d')
+        date  = '_' + date
     
-    data_set  = client.dataset(schema[file_type]['table_name'])
+    data_set  = client.dataset(dataset_id)
     table_ref = data_set.table(schema[file_type]['table_name'] + date)
     table     = bigquery.Table(table_ref)
     table.schema = file_schema['schema']
@@ -52,7 +60,7 @@ def create_table_ref(client, blob, schema):
     
     return table_ref
 
-def load_data_from_s3(client, blob, schema, config, overwrite = False):
+def load_data_from_s3(client, dataset_id, blob, schema, config, overwrite = False):
     
     job_config                   = bigquery.LoadJobConfig()
     job_config.sourceFormat      = config['sourceFormat']
@@ -62,37 +70,38 @@ def load_data_from_s3(client, blob, schema, config, overwrite = False):
         job_config.write_disposition = 'WRITE_TRUNCATE'
     
     s3_location      = "gs://{0}/{1}".format(blob.bucket.name, blob.name)
-    table_ref = create_table_ref(client, blob, schema)
+    table_ref = create_table_ref(client, dataset_id, blob, schema)
+    if if_tbl_exists(client, table_ref) & (overwrite == False):
+        return 1
     job       = client.load_table_from_uri(s3_location, table_ref, job_config = job_config)
     
     return job
 
 
-def create_base_datasets(client, blobs, schema):
-    
-    filenames = {}
-    for blob in blobs:
-        file = re.findall(r'/(.+)_', blob.name)
-        if file[0] in filenames:
-            filenames[file[0]] += 1
-        else:
-            filenames[file[0]]  = 1
-    
-    for key in filenames:
-        if key not in schema:
-            continue
-        if schema[key]['include'] == False:
-            continue
-        # The name for the new dataset - check if required
-        dataset_id  = schema[key]['table_name']
-        # Prepares a reference to the new dataset
-        dataset_ref = client.dataset(dataset_id)
-        dataset     = bigquery.Dataset(dataset_ref)
-        
-        # Creates the new dataset
-        dataset     = client.create_dataset(dataset)
-    
+def overwrite_dimensional_data(client, dataset_id, table_id, bucket, schema, config):
+    blobs    = bucket.list_blobs()
+    max_date = get_latest_s3_blob(table_id, blobs)
+    max_blob = bucket.get_blob("test/{0}_{1}.csv".format(table_id, max_date))
+    load_job = load_data_from_s3(bigquery_client, dataset_id, max_blob, schema, config, overwrite = True)
+    load_job.result()
+    print("loaded {0} lines of data".format(load_job.output_rows))
+    return 1
 
+def load_historical_data(client, dataset_id, table_id, bucket, schema, config, overwrite = False):
+    blobs    = bucket.list_blobs()
+    for blob in blobs:
+        file = re.findall(r'/(.+)_', blob.name)[0]
+        if file != table_id:
+            continue
+        print("loading {0} into big query".format(blob.name))
+        load_job = load_data_from_s3(client, dataset_id, blob, schema, config, overwrite = False)
+        load_job.result()
+        print("loaded {0} lines of data".format(load_job.output_rows))
+
+########################################
+########################################
+########################################
+        
 def get_latest_s3_blob(table_id, blobs):
     
     max_date  = '2000-00-00'
@@ -110,19 +119,6 @@ def get_latest_s3_blob(table_id, blobs):
     
     return max_date
 
-def overwrite_dimensional_data(table_id, client, bucket, schema, config):
-    blobs    = bucket.list_blobs()
-    max_date = get_latest_s3_blob(table_id, blobs)
-    max_blob = bucket.get_blob("test/{0}_{1}.csv".format(table_id, max_date))
-    load_job = load_data_from_s3(bigquery_client, max_blob, schema, config, overwrite = True)
-    load_job.result()
-    print("loaded {0} lines of data".format(load_job.output_))
-    return 1
-
-def load_historical_data(table_id, client, bucket, schema, config, overwrite = False):
-    blobs    = bucket.list_blobs()
-        file = re.findall(r'/(.+)_', blob.name)
-
 ########################################
 ########################################
 ########################################
@@ -131,11 +127,14 @@ bigquery_client = bigquery.Client()
 storage_client  = storage.Client.from_service_account_json('C:/usr/yeticrab/datacrab-045e6e03b60b.json')
 sojourn_bucket  = storage_client.bucket('sojourn')
 
-overwrite_dimensional_data('venues', bigquery_client, sojourn_bucket, schema, config)
-overwrite_dimensional_data('installations', bigquery_client, sojourn_bucket, schema, config)
 
-sojourn_blobs   = sojourn_bucket.list_blobs()
+create_dataset(bigquery_client, 'sojourn')
 
-########################################
-########################################
-########################################
+
+overwrite_dimensional_data(bigquery_client, 'sojourn', 'venues', sojourn_bucket, schema, config)
+overwrite_dimensional_data(bigquery_client, 'sojourn', 'installations', sojourn_bucket, schema, config)
+
+load_historical_data(bigquery_client, 'sojourn', 'stepschallenge', sojourn_bucket, schema, config)
+load_historical_data(bigquery_client, 'sojourn', 'DeviceLocations',  sojourn_bucket, schema, config)
+load_historical_data(bigquery_client, 'sojourn', 'visits', sojourn_bucket, schema, config)
+
